7935e0e008be40c4664515fed60c6e26f692a133
sprasad <sumit_prasad@localhost>
2015-02-26 17:24:52 +0900

    Deleted unnecessary files

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => '127.0.0.1',
-			'database'  => 'evote',
+			'database'  => 'schoolopedia',
 			'username'  => 'root',
 			'password'  => 'shivi',
 			'charset'   => 'utf8',
