a958bbe3b588f0097ac645ab358066f62aed316f
sumit singh <summmmit44@gmail.com>
2015-03-28 13:41:49 +0900

    users

--- app/views/user/classUsers.blade.php
+++ app/views/user/classUsers.blade.php
@@ -4,119 +4,6 @@
 @section('stylesheets')
 <link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/modifiedCss/user/classusers.css'); }}" />
 <style>
-
-
-    /* ============================================================
-      GLOBAL
-    ============================================================ */
-    .effects .img {
-        position: relative;
-        float: left;
-        margin-bottom: 5px;
-        width: 100%;
-        overflow: hidden;
-    }
-    .effects .img img {
-        display: block;
-        margin: 0;
-        padding: 0;
-        max-width: 100%;
-        height: auto;
-    }
-
-    .overlay {
-        display: block;
-        position: absolute;
-        z-index: 20;
-        background: rgba(0, 0, 0, 0.8);
-        overflow: hidden;
-        -webkit-transition: all 0.5s;
-        -moz-transition: all 0.5s;
-        -o-transition: all 0.5s;
-        transition: all 0.5s;
-    }
-
-    a.close-overlay {
-        display: block;
-        position: absolute;
-        top: 0;
-        right: 0;
-        z-index: 100;
-        width: 45px;
-        height: 45px;
-        font-size: 20px;
-        font-weight: 700;
-        color: #fff;
-        line-height: 45px;
-        text-align: center;
-        background-color: #000;
-        cursor: pointer;
-    }
-    a.close-overlay.hidden {
-        display: none;
-    }
-
-    a.expand {
-        display: block;
-        position: absolute;
-        z-index: 100;
-        width: 60px;
-        height: 60px;
-        border: solid 5px #fff;
-        text-align: center;
-        color: #fff;
-        line-height: 50px;
-        font-weight: 700;
-        font-size: 30px;
-        -webkit-border-radius: 30px;
-        -moz-border-radius: 30px;
-        -ms-border-radius: 30px;
-        -o-border-radius: 30px;
-        border-radius: 30px;
-    }
-
-
-    /* ============================================================
-      EFFECT 5 - ICON BORDER ANIMATE
-    ============================================================ */
-    #effect-5 .overlay {
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        opacity: 0;
-    }
-    #effect-5 .overlay a.expand {
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        margin: auto;
-        width: 100%;
-        height: 100%;
-        -webkit-border-radius: 0;
-        -moz-border-radius: 0;
-        -ms-border-radius: 0;
-        -o-border-radius: 0;
-        border-radius: 0;
-        -webkit-transition: all 0.5s;
-        -moz-transition: all 0.5s;
-        -o-transition: all 0.5s;
-        transition: all 0.5s;
-    }
-    #effect-5 .img.hover .overlay {
-        opacity: 1;
-    }
-    #effect-5 .img.hover .overlay a.expand {
-        width: 60px;
-        height: 60px;
-        -webkit-border-radius: 30px;
-        -moz-border-radius: 30px;
-        -ms-border-radius: 30px;
-        -o-border-radius: 30px;
-        border-radius: 30px;
-    }
-
 </style>
 @stop
 
@@ -148,109 +35,59 @@
             </div>
             <div class="panel-body">
                 <div class="row">
-                    <div class="col-md-3">
-                        <div id="effect-5" class="effects clearfix">
-                            <div class="img">
-                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/t-shirt.png" alt="" />
-                                <div class="overlay">
-                                    <a href="#" class="expand">+</a>
-                                    <a class="close-overlay hidden">x</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div id="effect-5" class="effects clearfix">
-                            <div class="img">
-                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/t-shirt.png" alt="" />
-                                <div class="overlay">
-                                    <a href="#" class="expand">+</a>
-                                    <a class="close-overlay hidden">x</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div id="product-card">
-                            <div id="product-front">
-                                <div id="effect-5" class="effects clearfix">
-                                    <div class="img">
-                                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/t-shirt.png" alt="" />
-                                        <div class="overlay">
-                                            <a href="#" class="expand">+</a>
-                                            <a class="close-overlay hidden">x</a>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="card-container manual-flip">
+                            <div class="card">
+                                <div class="front">
+                                    <div class="cover">
+                                        <img src="{{ URL::asset('assets/projects/images/covers/rotating_card_thumb.png'); }}"/>
+                                    </div>
+                                    <div class="user">
+                                        <img class="img-circle" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/t-shirt.png"/>
+                                    </div>
+                                    <div class="content">
+                                        <div class="main">
+                                            <h3 class="name">Sumit Singh</h3>
+                                            <p class="profession">20098087</p>
+                                        </div>
+                                        <div class="footer">
+                                            <button class="btn btn-simple" onclick="rotateCard(this)">
+                                                <i class="fa fa-mail-forward"></i> Manual Flip
+                                            </button>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="stats">        	
-                                    <div class="stats-container">
-                                        <span class="product_price">CS0157</span>
-                                        <span class="product_name">Sumit Singh</span>    
-                                        <p>Men's running shirt</p>                                                  
-
-                                        <div class="product-options">
-                                            <strong>SIZES</strong>
-                                            <span>XS, S, M, L, XL, XXL</span>
-                                            <strong>COLORS</strong>
-                                            <div class="colors">
-                                                <div class="c-blue"><span></span></div>
-                                                <div class="c-red"><span></span></div>
-                                                <div class="c-white"><span></span></div>
-                                                <div class="c-green"><span></span></div>
-                                            </div> 
-                                        </div>                        
-                                    </div>                         
-                                </div>
-                            </div>       
-                            <div id="product-back">
-
-                            </div>                     
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div id="product-card">
-                            <div id="product-front">
-                                <div id="effect-5" class="effects clearfix">
-                                    <div class="img">
-                                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/t-shirt.png" alt="" />
-                                        <div class="overlay">
-                                            <a href="#" class="expand">+</a>
-                                            <a class="close-overlay hidden">x</a>
+                                </div> <!-- end front panel -->
+                                <div class="back">
+                                    <div class="header">
+                                        <h5 class="motto">"Contact Details!"</h5>
+                                    </div>
+                                    <div class="content">
+                                        <div class="main">
+                                            <h4 class="text-center">Experince</h4>
+                                            <p>Mike was working with our team since 2012.</p>
+                                            <h4 class="text-center">Areas of Expertise</h4>
+                                            <p>Web design, Adobe Photoshop, HTML5, CSS3, Corel and many others...</p>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="stats">        	
-                                    <div class="stats-container">
-                                        <span class="product_price">CS0157</span>
-                                        <span class="product_name">Sumit Singh</span>    
-                                        <p>Men's running shirt</p>                                                  
-
-                                        <div class="product-options">
-                                            <strong>SIZES</strong>
-                                            <span>XS, S, M, L, XL, XXL</span>
-                                            <strong>COLORS</strong>
-                                            <div class="colors">
-                                                <div class="c-blue"><span></span></div>
-                                                <div class="c-red"><span></span></div>
-                                                <div class="c-white"><span></span></div>
-                                                <div class="c-green"><span></span></div>
-                                            </div> 
-                                        </div>                        
-                                    </div>                         
-                                </div>
-                            </div>       
-                            <div id="product-back">
-
-                            </div>                     
-                        </div>
-                    </div>
+                                    <div class="footer">
+                                        <button class="btn btn-simple" rel="tooltip" title="Flip Card" onclick="rotateCard(this)">
+                                            <i class="fa fa-reply"></i> Back
+                                        </button>
+                                        <div class="social-links text-center">
+                                            <a href="http://cretive-tim.com" class="facebook"><i class="fa fa-facebook fa-fw"></i></a>
+                                            <a href="http://cretive-tim.com" class="google"><i class="fa fa-google-plus fa-fw"></i></a>
+                                            <a href="http://cretive-tim.com" class="twitter"><i class="fa fa-twitter fa-fw"></i></a>
+                                        </div>
+                                    </div>
+                                </div> <!-- end back panel -->
+                            </div> <!-- end card -->
+                        </div> <!-- end card-container -->
+                    </div> <!-- end col sm 3 -->
                 </div>
             </div>
         </div>
     </div>
 </div>
-<!-- start: EXPORT DATA TABLE PANEL  -->
-<!-- end: EXPORT DATA TABLE PANEL -->
 <!-- End: Page Content -->
 @stop
 
@@ -263,41 +100,16 @@
         SVExamples.init();
     });
 </script>
-<script src="{{ URL::asset('assets/js/modifiedJs/modernizr.js'); }}"></script>
-<script>
-    $(document).ready(function() {
-        if (Modernizr.touch) {
-            // show the close overlay button
-            $(".close-overlay").removeClass("hidden");
-            // handle the adding of hover class when clicked
-            $(".img").click(function(e) {
-                if (!$(this).hasClass("hover")) {
-                    $(this).addClass("hover");
-                }
-            });
-            // handle the closing of the overlay
-            $(".close-overlay").click(function(e) {
-                e.preventDefault();
-                e.stopPropagation();
-                if ($(this).closest(".img").hasClass("hover")) {
-                    $(this).closest(".img").removeClass("hover");
-                }
-            });
+<script type="text/javascript">
+    function rotateCard(btn){
+        var $card = $(btn).closest('.card-container');
+        console.log($card);
+        if($card.hasClass('hover')){
+            $card.removeClass('hover');
         } else {
-            // handle the mouseenter functionality
-//            $().mouseenter(function() {
-//                console.log(this);
-//                $(this).addClass("animate");
-//                $(this).find(".img").addClass("hover");
-//            })
-//                    .mouseleave(function() {
-//                        $(this).removeClass("animate");
-//                        $(this).find(".img").removeClass("hover");
-//                    });
-            $("#product-card").each(function(i){
-                console.log(this);
-            });
+            $card.addClass('hover');
         }
-    });
+    }
 </script>
+<script src="{{ URL::asset('assets/js/modifiedJs/modernizr.js'); }}"></script>
 @stop
