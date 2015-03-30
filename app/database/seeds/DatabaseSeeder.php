df1d9587808c1dad0d8be120e58ac4126bcd2227
sprasad <sumit_prasad@localhost>
2015-03-30 16:52:27 +0900

    none

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -15,6 +15,15 @@ class DatabaseSeeder extends Seeder {
         $this->command->info('User table seeded!');
     	$this->call('UsersTableSeeder');
         
+		$this->call('ClassesTableSeeder');
+		$this->call('GroupsTableSeeder');
+		$this->call('PeriodsTableSeeder');
+		$this->call('SchoolTableSeeder');
+		$this->call('SchoolSessionTableSeeder');
+		$this->call('SectionsTableSeeder');
+		$this->call('StreamsTableSeeder');
+		$this->call('SubjectsTableSeeder');
+		$this->call('UsersToClassTableSeeder');
 	}
 
 }
