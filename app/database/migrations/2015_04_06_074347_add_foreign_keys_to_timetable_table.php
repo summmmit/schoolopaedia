d63a20952279e61ec5e3285550f7988e1d3cf918
sumit singh <summmmit44@gmail.com>
2015-04-07 23:07:14 +0900

    all finished

--- app/database/migrations/2015_04_06_074347_add_foreign_keys_to_timetable_table.php
+++ app/database/migrations/2015_04_06_074347_add_foreign_keys_to_timetable_table.php
@@ -16,7 +16,7 @@ class AddForeignKeysToTimetableTable extends Migration {
 		{
 			$table->foreign('subject_id', 'timetable_ibfk_1')->references('id')->on('subjects')->onUpdate('RESTRICT')->onDelete('RESTRICT');
 			$table->foreign('users_id', 'timetable_ibfk_2')->references('id')->on('users')->onUpdate('RESTRICT')->onDelete('RESTRICT');
-			$table->foreign('classes_id', 'timetable_ibfk_3')->references('id')->on('classes')->onUpdate('RESTRICT')->onDelete('RESTRICT');
+			$table->foreign('class_id', 'timetable_ibfk_3')->references('id')->on('classes')->onUpdate('RESTRICT')->onDelete('RESTRICT');
 			$table->foreign('section_id', 'timetable_ibfk_4')->references('id')->on('sections')->onUpdate('RESTRICT')->onDelete('RESTRICT');
 		});
 	}
