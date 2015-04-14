e5cd2e5511f12be9e3b3d487fccf30f02f30541b
sprasad <sumit_prasad@localhost>
2015-04-14 17:59:14 +0900

    schedule not finished

---
+++ app/database/migrations/2015_04_14_085841_add_foreign_keys_to_users_to_class_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddForeignKeysToUsersToClassTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users_to_class', function(Blueprint $table)
+		{
+			$table->foreign('user_id', 'users_to_class_ibfk_1')->references('id')->on('users')->onUpdate('RESTRICT')->onDelete('RESTRICT');
+			$table->foreign('class_id', 'users_to_class_ibfk_2')->references('id')->on('classes')->onUpdate('RESTRICT')->onDelete('RESTRICT');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users_to_class', function(Blueprint $table)
+		{
+			$table->dropForeign('users_to_class_ibfk_1');
+			$table->dropForeign('users_to_class_ibfk_2');
+		});
+	}
+
+}
